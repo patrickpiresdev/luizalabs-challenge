f5d5630549ae5de0826bd044d3f795670a6bfab5
Patrick Pires <patrick.piresp@gmail.com>
2022-06-29 22:29:55 -0300

    initial commit

---
+++ src/main/java/com/ptk/luizalabschallenge/LuizalabsChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.ptk.luizalabschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuizalabsChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuizalabsChallengeApplication.class, args);
+	}
+
+}
